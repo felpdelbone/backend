8409e19852400fdf68fb1fb0e2d3c03890d4d436
felpdelbone <felipe.delbone.senai404@gmail.com>
2024-08-02 10:45:49 -0300

    first code in php

--- php/index.php
+++ php/index.php
@@ -1,25 +1,18 @@
 <?php  
-$nome = 'felipe';
+// declaração das variaveis 
+$nome = 'felipe'; 
 $cidade  = 'cerquilho';
 $ce = 428 ; 
 $curso = 'desenvolvimento de sistema' ; 
 $media = 8.5 ; 
 
-echo 'Olá seja bem vindo ao SESI-SP <br>';
-
+echo 'Olá seja bem vindo ao SESI-SP <br>'; //introdução na tela 
+//aplicação das variaves 
 echo "$nome <br>";
 echo "$cidade <br>";
 echo "$ce <br>";
 echo "$curso <br>";
 echo "$media <br>";
 
-echo "olá $nome, seja bem vindo(a) ao SESI $ce da $cidade, sua media em $curso é $media" ;
+echo "olá $nome, seja bem vindo(a) ao SESI $ce da $cidade, sua media em $curso é $media" ; //aplicando a variavel de outra forma em uma introdução 
  ?>
-
-echo "# backend" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/felpdelbone/backend.git
-git push -u origin main
