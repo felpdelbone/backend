b83f708eb5de5b19e04d2ad7f23677cd5466c863
felpdelbone <felipe.delbone.senai404@gmail.com>
2024-08-07 16:38:02 -0300

    code in php

--- php/exc1.php
+++ php/exc1.php
@@ -1,12 +1,12 @@
 <?php
-
+//declaração das variaves 
 $semestre1 = 2;
 $semestre2 = 10;
 $media = ( $semestre1 + $semestre2 ) / 2;
-
+//exibe as variaveis 
 echo "1 Semestre: " . $semestre1 . '<br>';
 echo "2 Semestre: " . $semestre2 . '<br>';
-
+//faz o calculo para dar o resultado 
 if ($media >= 7){
     echo 'Você foi aprovado';
 }else{
