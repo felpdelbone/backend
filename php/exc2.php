175872a20087bf4ee98f00ed3ac7426ccc20a4b8
felpdelbone <felipe.delbone.senai404@gmail.com>
2024-08-09 09:56:40 -0300

    code in php

--- php/exc2.php
+++ php/exc2.php
@@ -1,2 +1,22 @@
 <?php
 
+// Declaração das variáveis
+$salario = 4000;
+$horas_extras = 2;
+
+// Inicialização do salário total com o salário base
+$salario_total = $salario; 
+
+// Aplicando as variáveis e fazendo a verificação
+if ($salario < 2000 && $horas_extras >= 10) {
+    $adicional = $salario * 0.20; 
+    $salario_total += $adicional;
+    echo "O seu salário esse mês foi de R$ " .$salario_total. "<br>";
+    echo "Parabéns, você teve 20% a mais do seu salário.";
+} else {
+    $adicional = $salario * 0.10;
+    $salario_total += $adicional;
+    echo "O seu salário esse mês foi de R$ " .$salario_total. "<br>";
+    echo "Parabéns, você teve 10% a mais do seu salário.";
+}
+?>
---
+++ php/exc3.php
@@ -0,0 +1,12 @@
+<?php 
+//declarando variavel
+$valor_compra = 500;
+$vip = 'nao'; 
+//if else sendo aplicado 
+if($valor_compra >= 500 && $vip = 'sim'){
+    echo 'O cliente recebe o desconto';
+}else{
+    echo 'o cliente não recebe o desconto';
+}
+
+?>
