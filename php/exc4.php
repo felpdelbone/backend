e9009b7ff184b6f0e058375f5932a014ba8d5fcb
felpdelbone <felipe.delbone.senai404@gmail.com>
2024-08-14 16:25:23 -0300

    code in php

---
+++ php/exc4.php
@@ -0,0 +1,26 @@
+<?php
+
+
+for ($i =1; $i <= 10; $i++) {
+    echo $i . " ";
+    echo ($i % 2 != 0) ? ' (ímpar)' : ' (par)';
+    echo " - ";
+
+    switch (true) {
+        case ($i >= 1 && $i <= 3):
+            echo 'baixo';
+            break;
+        case ($i >= 4 && $i <= 7):
+            echo 'médio';
+            break;
+        case ($i >= 8 && $i <= 10):
+            echo 'alto';
+            break;
+        default:
+            echo 'Numero nao reconhecido';
+    }
+
+    echo "<br>";
+}
+?>
+
