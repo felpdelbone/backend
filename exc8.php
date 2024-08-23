933454071ad7a71fb955b3b8e9c993aa3b363b6e
felpdelbone <felipe.delbone.senai404@gmail.com>
2024-08-23 10:41:10 -0300

    code in php

---
+++ exc8.php
@@ -0,0 +1,11 @@
+<?php
+
+$contador = 0;
+
+for ($i = 1;$i  <= 100; $i++) {
+    $contador = ($contador + $i);
+}
+
+echo "O Vaolro de i: $contador<br>";    
+
+?>
---
+++ exc9.php
@@ -0,0 +1,11 @@
+<?php
+
+$contador = 0;
+for($i = 1; $i <= 50; $i ++){''
+    $contador = sqrt($i);
+    $arredondar = round($contador, 2);
+    echo "a raiz quadrada é: $arredondar<br>";
+}
+
+
+?>
